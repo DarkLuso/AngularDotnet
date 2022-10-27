ad2a0a928613e522a6aef990fcdfe775ca9ebee5
Darckluso <darckluso@gmail.com>
2022-10-26 21:27:06 -0300

    Tables

--- SmartSchool/src/app/alunos/alunos.component.ts
+++ SmartSchool/src/app/alunos/alunos.component.ts
@@ -10,13 +10,13 @@ export class AlunosComponent implements OnInit {
   public titulo = "Alunos";
 
   public alunos = [
-  {nome: "Marta"},
-  {nome: "Paula"},
-  {nome: "Laura"},
-  {nome: "Luiza"},
-  {nome: "Lucas"},
-  {nome: "Pedro"},
-  {nome: "Paulo"}
+  {id: '1',nome: "Marta", sobrenome: 'Kent', telefone: '999332255'},
+  {id: '2',nome: "Paula", sobrenome: 'Isabela', telefone: '993354288'},
+  {id: '3',nome: "Laura", sobrenome: 'Antonia', telefone: '955668899'},
+  {id: '4',nome: "Luiza", sobrenome: 'Maria', telefone: '965656599'},
+  {id: '5',nome: "Lucas", sobrenome: 'Machado', telefone: '993328753'},
+  {id: '6',nome: "Pedro", sobrenome: 'Alvares', telefone: '999324507'},
+  {id: '7',nome: "Paulo", sobrenome: 'José', telefone: '918319967'}
   ]
 
   constructor() { }
--- SmartSchool/src/app/professores/professores.component.ts
+++ SmartSchool/src/app/professores/professores.component.ts
@@ -10,11 +10,11 @@ export class ProfessoresComponent implements OnInit {
   public titulo = "Professores";
 
   public professores = [
-    {nome: "Legolas"},
-    {nome: "Ricardo"},
-    {nome: "Dagoth Ur"},
-    {nome: "Barba Negra"},
-    {nome: "Ben 10"},    
+    {id: '1', nome: "Legolas", disciplina: 'Arquearia'},
+    {id: '2', nome: "Ricardo", disciplina: 'Matematica'},
+    {id: '3', nome: "Dagoth Ur", disciplina: 'Cultos das Trevas'},
+    {id: '4', nome: "Barba Negra", disciplina: 'Pirataria'},
+    {id: '5', nome: "Ben 10", disciplina: 'Heroismo'},    
 
   ]
 
